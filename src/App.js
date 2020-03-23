211d4bb7ca8eb1af64256264789373074f61fbb9
Obaida Alhassan <obaida@terkwaz.com>
2020-03-23 19:00:52 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          the magic is coming soon
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          click here :) to know more...
         </a>
       </header>
     </div>
